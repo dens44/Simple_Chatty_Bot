1c69f8670c80aeb30082c06f73c7ee27bebae954
dsmirnov <d.smirnov44@gmail.com>
2020-10-08 02:03:26 +0300

    Initial commit

---
+++ Problems/Division/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static double divide(long a, long b) {
+      if (b!=0)  return (double) (a)/b ; // write your code here
+    else return (double) 0 ;}
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final long a = scanner.nextLong();
+        final long b = scanner.nextLong();
+        System.out.println(divide(a, b));
+    }
+}
